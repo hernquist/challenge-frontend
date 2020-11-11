bc1c3f853e1f1a41c41be4ffa3f0779d26268b47
David Hernquist <david.hernquist@turner.com>
2020-11-11 10:52:48 -0500

    css for cards

--- components/cards/styles.js
+++ components/cards/styles.js
@@ -2,19 +2,27 @@ import styled from "styled-components";
 import { FRACTIONS } from "../../constant";
 
 export const LargeCardContainer = styled.div`
-  margin: 1rem;
-  height: 20rem;
-  width: 12rem;
+  margin: 0.6rem;
+  height: 10rem;
+  width: 6rem;
+  border-radius: 0.6rem;
   background: ${({ theme }) => theme.color.pomengranate};
-  border: 4px solid white;
-  font-size: ${({ topic }) => (topic === FRACTIONS ? "6rem" : "4.2rem")};
+  border: 3px solid white;
+  font-size: ${({ topic }) => (topic === FRACTIONS ? "3rem" : "2.1rem")};
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-radius: 1rem;
   color: ${({ theme }) => theme.color.offBlack};
   box-shadow: ${({ theme }) => theme.boxShadow.primaryCard};
+
+  ${({ theme }) => theme.mediaQuery.tablet} {
+    margin: 1rem;
+    height: 20rem;
+    width: 12rem;
+    border-radius: 1rem;
+    font-size: ${({ topic }) => (topic === FRACTIONS ? "6rem" : "4.2rem")};
+  }
 
   &:hover,
   &:focus {
