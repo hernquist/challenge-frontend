db0941e7e026e5b38aba4e7a954155264e8c189f
David Hernquist <david.hernquist@turner.com>
2020-11-11 12:30:55 -0500

    fixing page height

--- components/shell/styles.js
+++ components/shell/styles.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+  min-height: 100%;
+`;
+
+export const Body = styled.body`
+  margin: 0;
+  height: 100vh;
   background-color: ${({ theme }) => theme.color.iceberg};
-  height: 100%;
+  overflow-y: hidden;
 `;
